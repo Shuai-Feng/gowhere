0e342231420e8625c230b20000101e796ae2de24
tom <shuaifeng12138@gmail.com>
2020-06-15 05:23:28 +0800

    haha

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        大哥喝可乐
+        <router-link to="/list">列表页</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="less" scoped>
+
+</style>
