7cd75db82e59e51a223643010957cb4819ec94f6
tom <shuaifeng12138@gmail.com>
2020-06-17 17:05:55 +0800

    city ajax

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -44,40 +44,41 @@
 
 <style lang="stylus" scoped>
     @import '~styles/varibles.styl';
-    .border-topbottom
-     &:before
-        border-color #777
-     &:after
-        border-color #777
-    .list
-        position absolute
-        top 1.48rem
-        left 0
-        right 0
-        bottom 0
-
-        // overflow hidden
-        .title 
-            line-height .4rem
-            background #EEE
-            padding-left .2rem  
-            color #666
-            font-size .26rem
-        .button-list
-            overflow hidden
-            padding .1rem
-            .button-wrapper
-                float left
-                width 33.33%
-                .button
-                    padding .1rem 0
-                    margin .1rem
-                    text-align center
-                    border .02rem solid #ccc
-                    border-radius .06rem
-        .item-list
-            .item
-                line-height .54rem
-                color #666
-                padding .08rem .2rem 
+  .border-topbottom
+    &:before
+      border-color: #ccc
+    &:after
+      border-color: #ccc
+  .border-bottom
+    &:before
+      border-color: #ccc
+  .list
+    overflow: hidden
+    position: absolute
+    top: 1.58rem
+    left: 0
+    right: 0
+    bottom: 0
+    .title
+      line-height: .54rem
+      background: #eee
+      padding-left: .2rem
+      color: #666
+      font-size: .26rem
+    .button-list
+      overflow: hidden
+      padding: .1rem .6rem .1rem .1rem
+      .button-wrapper
+        float: left
+        width: 33.33%
+        .button
+          margin: .1rem
+          padding: .1rem 0
+          text-align: center
+          border: .02rem solid #ccc
+          border-radius: .06rem
+    .item-list
+      .item
+        line-height: .76rem
+        padding-left: .2rem
 </style>
